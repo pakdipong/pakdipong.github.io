cb1770ea01f8eed0fc1d609cf1a85cc94681e7bb
pakdipong <157682440+pakdipong@users.noreply.github.com>
2024-04-21 00:08:48 +0700

    Carousel

---
+++ slider.js
@@ -0,0 +1,19 @@
+const slider = document.querySelector('.slider')
+const firstSlide = slider.querySelectorAll('.item')[0]
+const arrowIcons = document.querySelectorAll('.new-products span')
+
+let firstSlideWidth = firstSlide.clientWidth
+
+window.addEventListener("resize", () => {
+    firstSlideWidth = firstSlide.clientWidth
+})
+
+arrowIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+        if (icon.id === 'right') {
+            slider.scrollLeft += firstSlideWidth
+        } else {
+            slider.scrollLeft -= firstSlideWidth
+        }
+    })
+})
