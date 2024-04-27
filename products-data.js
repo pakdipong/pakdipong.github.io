cdeec0e27afe6c64c68a2fc62410f89989afd9fe
pakdipong <157682440+pakdipong@users.noreply.github.com>
2024-04-27 22:30:53 +0700

    products-data

---
+++ products-data.js
@@ -0,0 +1,110 @@
+export const products = [
+    {
+        id: 1,
+        name: 'Nike V2K Run',
+        type: 'Shoes',
+        price: 120,
+        imageUrl: './assets/products/รองเท้า-v2k-run-zJV8TV.png',
+        about: 'Fast forward. Rewind. Doesn’t matter—this shoe takes retro into the future. The V2K remasters everything you love about the Vomero in a look pulled straight from an early aughts running catalog. Layer up in a mixture of flashy metallics, referential plastic details and a midsole with a perfectly vintage aesthetic. And the chunky heel makes sure wherever you go, it is in comfort.',
+        review: 11
+    },
+    {
+        id: 2,
+        name: 'Nike Killshot 2 Leather',
+        type: 'Shoes',
+        price: 90,
+        imageUrl: './assets/products/killshot-2-leather-shoes-72M7D1.png',
+        about: "Inspired by the original low-profile tennis shoes, the Nike Killshot 2 updates the upper with a variety of textured leathers to create a fresh look. From soft suedes to smooth leathers with the perfect sheen, it's court-side attitude with a modern touch. To prove you're on top, the rubber gum sole adds the cherry on bottom.",
+        review: 15
+    },
+    {
+        id: 3,
+        name: 'Nike Vaporfly 3',
+        type: "Men's Road Racing Shoes",
+        price: 260,
+        imageUrl: './assets/products/vaporfly-3-mens-road-racing-shoes-8DD182.png',
+        about: "Catch ‘em if you can. Giving you race-day speed to conquer any distance, the Nike Vaporfly 3 is made for the chasers, the racers, the elevated pacers who can’t turn down the thrill of the pursuit. We reworked the leader of the super shoe pack and tuned the engine underneath to help you chase personal bests from a 10K to marathon. From elite runners to those new to racing, this versatile road-racing workhorse is for those who see speed as a gateway to more miles and more seemingly uncatchable highs.",
+        review: 30
+    },
+    {
+        id: 4,
+        name: 'Nike Tiempo Emerald Legend 10 Pro',
+        type: 'FG Low-Top Soccer Cleats',
+        price: 140,
+        imageUrl: './assets/products/tiempo-emerald-legend-10-pro-fg-low-top-soccer-cleats-22lhdW.png',
+        about: "Serious about your game? We make Pro cleats for those looking to take their game to the next level. Celebrating 30 years of Tiempo, this Legend 10 references old Tiempo elements that are part of Nike’s DNA along with futuristic textures and graphics. Add in FlyTouch Pro and All Conditions Control (a grippy texture even in wet weather), and you’re ready to leave your mark over the next three decades.",
+        review: 9
+    },
+    {
+        id: 5,
+        name: 'Nike Phantom GX 2 Elite',
+        type: 'FG Low-Top Soccer Cleats',
+        price: 260,
+        imageUrl: './assets/products/phantom-gx-2-elite-fg-low-top-soccer-cleats-lSxqH0.png',
+        about: "Obsessed with perfecting your craft? We made this for you. In the middle of the storm, with chaos swirling all around you, you’ve calmly found the final third of the field, thanks to your uncanny mix of on-ball guile and grace. Go finish the job in the Phantom GX 2 Elite. We design Elite cleats for you and the world’s biggest stars to give you high-level quality, because you demand greatness from yourself and your footwear.",
+        review: 5
+    },
+    {
+        id: 6,
+        name: 'Nike Winflo 9 Shield',
+        type: "Men's Weatherized Road Running Shoes",
+        price: 115,
+        imageUrl: './assets/products/wio-9-shield-mens-weatherized-road-running-shoes-bzxswh.png',
+        about: "Nasty weather doesn’t need to put an end to your run. Revel in the elements with these shoes designed to help you push through every puddle, pit stop, pothole and personal record. A water-repellent finish helps keep you dry while tire-like traction and a full-length Air unit provide grip and soft cushioning to help keep you running when conditions get slick.",
+        review: 2
+    },
+    {
+        id: 7,
+        name: "Nike Air Max Plus",
+        type: "Men's Shoes",
+        price: 180,
+        imageUrl: './assets/products/air-max-plus-mens-shoes-x9G2xF.png',
+        about: "Let your attitude have the edge with flame-like caging that adds heat to the streets while airy mesh keeps you cool. The Nike Air Max Plus gives you a tuned Nike Air experience that offers premium stability and unbelievable cushioning.",
+        review: 20
+    },
+    {
+        id: 8,
+        name: "Nike Air Force 1 '07",
+        type: "Men's Shoes",
+        price: 115,
+        imageUrl: './assets/products/air-force-1-07-mens-shoes-J7xw5P.png',
+        about: "Comfortable, durable and timeless—it’s number 1 for a reason. The ‘80s construction pairs with classic colors for style that tracks whether you’re on court or on the go.",
+        review: 9
+    },
+    {
+        id: 9,
+        name: "Nike Air Max Dn",
+        type: "Shoes",
+        price: 160,
+        imageUrl: './assets/products/air-max-dn-shoes-27XkSQ.png',
+        about: "Feel unbeatable, from the tee box to the final putt. Inspired by one of the most iconic sneakers of all time, the Air Jordan 1 G is an instant classic on the course. With Air cushioning underfoot, a Wings logo on the heel and an integrated traction pattern to help you power through your swing, it delivers all the clubhouse cool of the original AJ1—plus everything you need to play 18 holes in comfort.",
+        review: 30
+    },
+    {
+        id: 10,
+        name: "Nike Air Max2 CB '94",
+        type: "Men's Shoes",
+        price: 170,
+        imageUrl: './assets/products/air-max2-cb-94-mens-shoes-rtgvnr.png',
+        about: "The Air Max2 CB '94 returns with more force than ever. This high-top take on the basketball classic speaks for itself by combining attitude with a fierce game. It delivers the same backboard-shattering style and redefined comfort as it did in the '90s.",
+        review: 1
+    },
+    {
+        id: 11,
+        name: "Nike Motiva",
+        type: "Men's Walking Shoes",
+        price: 110,
+        imageUrl: './assets/products/motiva-mens-walking-shoes-nPmqDJ.png',
+        about: "The Nike Motiva helps you step through whatever the day brings, at your pace. Its uniquely patterned outsole and exaggerated rocker combine to give you a super-smooth, cushioned and comfortable ride. This means you can walk, jog or run comfortably and come back for your next leisurely stroll confidently. It gives you optimal support for your every move, every day.",
+        review: 54
+    },
+    {
+        id: 12,
+        name: "Nike Zoom Fly 5",
+        type: "Men's Road Running Shoes",
+        price: 170,
+        imageUrl: './assets/products/zoom-fly-5-mens-road-running-shoes-jGsdSl.png',
+        about: "Bridge the gap between your weekend training run and race day in a durable design that can be deployed not just at the starting line of your favorite race but in the days and months after your conquest. It offers comfort and reliability but with a propulsive sensation that’ll help you feel fast and fresh. That kind of versatility is uncommon in the running arena. But who said you can’t have it all?",
+        review: 49
+    }
+]
